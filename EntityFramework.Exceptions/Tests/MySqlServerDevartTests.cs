1875963ba191026f1e168d7997d953d820aa1820
Jan Trejbal <jan@trejbal.land>
2026-05-22 17:05:36 +0200

    Add MySqlServerDevartTests

---
+++ EntityFramework.Exceptions/Tests/MySqlServerDevartTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Devart.Data.MySql;
+using EntityFramework.Exceptions.MySQL.Devart;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MySql;
+using Xunit;
+
+namespace EntityFramework.Exceptions.Tests;
+
+[Collection("MySql Test Collection")]
+public class MySqlServerDevartTests : DatabaseTests, IClassFixture<MySqlDevartDemoContextFixture>
+{
+    public MySqlServerDevartTests(MySqlDevartDemoContextFixture fixture) : base(fixture.DemoContext)
+    {
+    }
+}
+
+public class MySqlDevartDemoContextFixture : DemoContextFixture<MySqlContainer>
+{
+    static MySqlDevartDemoContextFixture()
+    {
+        Container = new MySqlBuilder().Build();
+    }
+
+    private static string AddLicenceKeyToTheConnectionString(
+        string connectionString) =>
+        new MySqlConnectionStringBuilder(connectionString)
+        {
+            LicenseKey = Environment.GetEnvironmentVariable("DEVART_LICENSE")
+        }.ConnectionString;
+
+    protected override DbContextOptionsBuilder<DemoContext> BuildDemoContextOptions(
+        DbContextOptionsBuilder<DemoContext> builder, string connectionString)
+    {
+        builder.UseMySql(AddLicenceKeyToTheConnectionString(connectionString));
+
+        return builder.UseExceptionProcessor();
+    }
+
+    protected override DbContextOptionsBuilder BuildSameNameIndexesContextOptions(
+        DbContextOptionsBuilder builder, string connectionString)
+        => builder.UseMySql(AddLicenceKeyToTheConnectionString(connectionString)).UseExceptionProcessor();
+}
